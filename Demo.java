f4e612901ce88bede6f03f54f12e4d72fb75e406
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-03 13:48:45 +0200

    CH

--- Demo.java
+++ Demo.java
@@ -1,7 +1,13 @@
-class Demo{       // CG
+class Demo{       // CH
 	public static void main(String[] ebc){
-		GGG ggg = new GGG();
-		ggg.ggg();
+		GGG ggg = new UUU();
+		try {
+			ggg.ggg();
+		} catch (RuntimeException e) {
+			System.out.println("iets");
+		} finally {
+			System.out.println("niets");
+		}
 		System.out.println("Prachtig");
 	}
 }
@@ -14,6 +20,8 @@ class GGG {
 
 class UUU extends GGG {
 	void ggg() {
-		throw new RuntimeException();
+		throw new AB();
 	}
 }
+
+class AB extends RuntimeException(){}
