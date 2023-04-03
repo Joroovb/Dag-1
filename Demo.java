843d5982f7a7d2d1de5bcdcd72fb4ec1d512040a
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-03 13:17:47 +0200

    BI

--- Demo.java
+++ Demo.java
@@ -1,10 +1,7 @@
-class Demo{       // BG
+class Demo{       // BI
 	public static void main(String[] ebc){
 	   Maker m = new Maker();
-	   m.gaan().voortgaan();
-	   Fiets f = new Fiets();
-	   System.out.println(m.gaan().a++);
-	   System.out.println(m.gaan().a++);
+	   (Fiets) m.gaan().voortgaan();
 	}
 }
 class Fiets extends Voertuig {
