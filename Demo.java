2eadca3bd3944b053f4ff256ee6dc9eda43196c3
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-03 10:24:38 +0200

    AI

--- Demo.java
+++ Demo.java
@@ -1,5 +1,19 @@
-class Demo{       // AF
-	public static void main(String... ebc){
-	   System.out.println("Let's go!");
+class Demo{       // AI
+	public static void main(String[] ebc){
+	    System.out.println("Let's go!");
+		Boven b = new Onder();
+		b.hoi();
 	}
+}
+
+class Boven {
+    void hoi() {
+        System.out.println("Ik ben boven");
+    }
+}
+
+class Onder extends Boven {
+    void hoi() {
+        System.out.println("ik ben beneden");
+    }
 }
