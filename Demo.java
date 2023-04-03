20282a256812b20482f96b8f4ed4bcf2b81cb4c4
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-03 10:45:51 +0200

    BC

--- Demo.java
+++ Demo.java
@@ -1,19 +1,20 @@
-class Demo{       // BB
+class Demo{       // BC
 	public static void main(String[] ebc){
 	    System.out.println("maandag");
 		Fiets f = new Fiets();
 		f.mmh = 3;
 		Fiets f2 = new Fiets();
 		System.out.println(f2.mmh);
-		System.out.println(f2.hmm);
 	}
 }
-
 class Fiets {
     int hmm;
-    static int mmh;
-
-    Fiets() {
+    static int mmh = jo();
+    void Fiets() {
         System.out.println("vrijdag");
+    }
+    int jo() {
+        System.out.println("5");
+        return 6;
     }
 }
