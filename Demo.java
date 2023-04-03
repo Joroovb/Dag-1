aa449312affe370d003aab9f1e3d84c2cb447e5e
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-03 10:13:50 +0200

    AD

--- Demo.java
+++ Demo.java
@@ -1,9 +1,21 @@
-class Demo{       // AB
+class Demo{       // AD
+    static int a = hoi();
+    int b = doei();
 	public static void main(String args[]){
 		System.out.println("hoi");
-		go();
+	    new Demo().go();
+		new Demo().go();
 	}
 	public void go(){
 		System.out.println("doei");
+	}
+
+	int hoi() {
+	    System.out.println("oeps");
+		return 7;
+	}
+	int doei(){
+	    System.out.println("anders");
+		return 7;
 	}
 }
