02a79d348e93c091851bba234caf2e333cfa1571
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-06 10:12:45 +0200

    EF

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
-class Demo{       // EE
+class Demo{       // EF
 	public static void main(String[] args) {
-        int[] anders = new int[];
-        System.out.println("GO");
+        int[] anders = new int[4];
+        System.out.println(anders[4]);
 	}
 }
