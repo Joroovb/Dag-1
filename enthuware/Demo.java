0931ef9f9adb2fed6006af097e8ec95d520f94bc
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-13 10:32:40 +0200

    QC

--- enthuware/Demo.java
+++ enthuware/Demo.java
@@ -1,14 +1,14 @@
-public class Demo{   // QB
+public class Demo{   // QC
 	public static void main(String[] args){
 		System.out.println("Hij doet het");
-		Jas jas = new Jas();
-		Jas jas2 = new Jas();
+        Jas jas = new Jas();
+        Jas jas2 = new Jas();
 	}
 }
 
 
 class Jas{
-	static int knoop = knopen();
+	int knoop = knopen();
 	static int knopen(){
 		System.out.println("in knopen");
 		return 17;
