e7cf426ce5a751c5c6478326e3e3ee1bffb0b4a2
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-13 11:48:33 +0200

    UQ

--- enthuware/Demo.java
+++ enthuware/Demo.java
@@ -1,14 +1,19 @@
-public class Demo{   // UN
+public class Demo{   // UQ
 	public static void main(String[] args) {
 	    Fiets f = new Fiets();
+		Fiets f2 = f;
         lopen(f);
-        System.out.println("einde");
+        System.out.println("einde" + f2.a);
 	}
 	static void lopen(Object x) {
+	   x.a = 25;
 	   System.out.println("We zijn hier");
 	}
 }
-
 class Fiets {
-
+    int a = hoi();
+    static int hoi() {
+        System.out.println("Hoi!");
+        return 7;
+    }
 }
