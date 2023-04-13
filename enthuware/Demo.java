028775a54ff7d61ccd7f5e7883a678175713f941
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-13 11:31:57 +0200

    MB

--- enthuware/Demo.java
+++ enthuware/Demo.java
@@ -1,4 +1,4 @@
-public class Demo{   // MA
+public class Demo{   // MB
 	public static void main(String[] args){
         System.out.println("a");
         B b = new B();
@@ -10,6 +10,6 @@ class B {
     int b;
     void ben() {
         System.out.println("Ben toch");
-        throw new RuntimeException();
+        throw new Exception();
     }
 }
