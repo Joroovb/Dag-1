4945beb66cf46c2ee6ef0c8221f0fc86c2b96417
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-13 10:50:58 +0200

    QI

--- enthuware/Demo.java
+++ enthuware/Demo.java
@@ -1,28 +1,21 @@
-public class Demo{   // QG
+public class Demo{   // QI
 	public static void main(String[] args){
-		System.out.println("Hij doet het");
-        Jas jas = new Jas();
+        Kleding jas = new Jas();
         System.out.println(jas.prijs);
         jas.knopen();
 	}
 }
 class Jas extends Kleding {
     int knoop = 55;
+    int prijs = 88;
 	int knopen(){
 		System.out.println("in knopen");
 		return 17;
 	}
-	Jas(){
-	    super(5);
-		System.out.println("in Jas");
-	}
 }
 class Kleding {
     int prijs;
-    Kleding (int getal) {
-        System.out.println("In kleding");
-        prijs = getal;
-    }
+    int knoop = 70;
     int knopen() {
         System.out.println("In knopen kleding");
         return 18;
