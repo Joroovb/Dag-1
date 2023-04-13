1f308c2aeecb82dccdb8cf1885968bc235ab4e72
Joroovb <jorisvanbreugel@mailbox.org>
2023-04-13 12:00:42 +0200

    UT

--- enthuware/Demo.java
+++ enthuware/Demo.java
@@ -1,19 +1,17 @@
-public class Demo{   // UR
+public class Demo{   // UT
 	public static void main(String[] args) {
-	    Fiets f = new Fiets();
-		Fiets f2 = f;
-        lopen(f);
-        System.out.println("einde" + f2.a);
-	}
-	static void lopen(Fiets x) {
-	   x.a = 25;
-	   System.out.println("We zijn hier");
+        FietsenMaker piet = new FietsenMaker();
+	    Fiets f = piet.maken(60);
+		System.out.println("einde" + f.snelheid);
 	}
 }
-class Fiets {
-    int a = hoi();
-    static int hoi() {
-        System.out.println("Hoi!");
-        return 7;
+class FietsenMaker{
+    Fiets maken(int q) {
+        Fiets t = new Fiets();
+        t.snelheid = q + 10;
+        return t;
     }
+}
+class Fiets {
+    int snelheid;
 }
